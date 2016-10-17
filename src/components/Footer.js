9aebb037e4c176402f2695216bef97a631e90d59
Michael-André Joda <info@mikedre.com>
2016-10-17 17:54:38 +0100

    Added in Bootstrap, added in base head elements

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <footer className="wrapper__footer">
+            <div className="container">Footer Component</div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <header className="wrapper__header">
+            <div className="container">Header Component</div>
+        </header>
+    )
+}
+
+export default Header;
